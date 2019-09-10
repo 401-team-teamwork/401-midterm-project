f549bf1d4760017c988f8dccd9f0472a4b2ef520
lilbonito <sauteboy@gmail.com>
2019-09-10 16:53:08 -0700

    user login/password

---
+++ src/userTest.js
@@ -0,0 +1,21 @@
+const getUserNameAndPassword = require('./userPrompts').getUserNameAndPassword;
+const clear = require('clear');
+const figlet = require('figlet');
+// const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+
+
+clear();
+console.log(
+  chalk.blueBright(
+figlet.textSync('SUPERTYPE:    REVOLUTION', {font:'ANSI Shadow', horizontalLayout: 'full' })
+  )
+);
+
+const run = async () => {
+const userStuff = await getUserNameAndPassword();
+  console.log(userStuff);
+}
+run();
+
